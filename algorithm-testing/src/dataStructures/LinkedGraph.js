3c6c4d56f0e2e3b36b377f38e82cb8fb6ca676ec
Dakota Lawson <lawsonde@dukes.jmu.edu>
2025-03-27 09:11:52 -0400

    graph data structure created

---
+++ algorithm-testing/src/dataStructures/LinkedGraph.js
@@ -0,0 +1,46 @@
+class LinkedGraph {
+    constructor() {
+        this.nodes = new Map(); // Map to store nodes by their unique identifiers
+    }
+
+    addNode(id) {
+        if (!this.nodes.has(id)) {
+            this.nodes.set(id, new Set()); // Each node points to a set of connected nodes
+        }
+    }
+
+    addEdge(fromId, toId) {
+        if (!this.nodes.has(fromId) || !this.nodes.has(toId)) {
+            throw new Error("Both nodes must exist before adding an edge.");
+        }
+        this.nodes.get(fromId).add(toId); // Add a directed edge from 'fromId' to 'toId'
+    }
+
+    removeEdge(fromId, toId) {
+        if (this.nodes.has(fromId)) {
+            this.nodes.get(fromId).delete(toId); // Remove the directed edge
+        }
+    }
+
+    removeNode(id) {
+        if (this.nodes.has(id)) {
+            this.nodes.delete(id); // Remove the node itself
+            for (const [nodeId, edges] of this.nodes) {
+                edges.delete(id); // Remove all edges pointing to this node
+            }
+        }
+    }
+
+    getNeighbors(id) {
+        if (!this.nodes.has(id)) {
+            throw new Error("Node does not exist.");
+        }
+        return Array.from(this.nodes.get(id)); // Return neighbors as an array
+    }
+
+    hasEdge(fromId, toId) {
+        return this.nodes.has(fromId) && this.nodes.get(fromId).has(toId);
+    }
+}
+
+module.exports = LinkedGraph;
